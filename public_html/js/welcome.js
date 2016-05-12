5b6bea81d489fbc4ed27bff05429e8d7ee17c864
Buranzo <buranzo@gmail.com>
2016-05-12 13:16:02 +0300

    Has been added table function

--- public_html/js/welcome.js
+++ public_html/js/welcome.js
@@ -5,3 +5,16 @@ function welcome() {
     welcomeElement.value = 'welcome';
     alert('start script change method: ' + welcomeElement.value);
 }
+
+function test(cols, rows) {
+//    rows = 3;
+//    cols = 3;
+    for (row = 0; row < rows; row++) {
+        for (col = 0; col < cols; col++) {
+            document.write("col: " + col + " |   row: " + row);
+            document.write("<br>")
+        }
+        document.write("<br>")
+        document.write("<br>")
+    }
+}
